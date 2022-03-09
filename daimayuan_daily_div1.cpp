55302c62b12d7ebbcc55691c58e5ace4192d0613
juruo-c <73051916+juruo-c@users.noreply.github.com>
2022-03-09 20:56:12 +0800

    22.3.9

--- daimayuan_daily_div1.cpp
+++ daimayuan_daily_div1.cpp
@@ -1,59 +1,63 @@
-//http://oj.daimayuan.top/course/10/problem/497
+//http://oj.daimayuan.top/course/10/problem/469
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
-typedef long long LL;
+const int N = 500005;
 
-const int N = 300005, M = 32;
-
-int n;
+int n, m;
 int a[N];
-int ch[N * M][2], cnt;
-int id[N][M];
-int s1[N * M], s0[N * M];
+map<int, int> last;
+int l[N], r[N];
+int cnt;
 
-void add(int e, int x)
+int f[N][30];
+int lg[N];
+
+int ask(int l, int r)
 {
-    int u = 0;
-    for (int i = 31; i >= 0; i -- )
-    {
-        int v = x >> i & 1;
-        if (ch[u][v] == 0) ch[u][v] = ++ cnt;
-        u = ch[u][v];
-        id[e][i] = u;
-    }
+    int k = lg[r - l + 1];
+    return  min(f[l][k], f[r - (1 << k) + 1][k]);
 }
 
 int main()
 {
-    scanf("%d", &n);
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i ++ )
     {
         scanf("%d", &a[i]);
-        add(i, a[i]);
-    }
-
-    LL ans = 0;
-    int x = 0;
-    for (int i = 31; i >= 0; i -- )
-    {
-        LL c1 = 0, c0 = 0;
-        for (int j = 1; j <= n; j ++ )
+        if (last[a[i]])
         {
-            int t = id[j][i + 1];
-            if (a[j] & (1 << i))
-                c1 += s0[t], s1[t] ++;
-            else c0 += s1[t], s0[t] ++;
+            int t = last[a[i]];
+            if (t > l[cnt])
+            {
+                l[++ cnt] = t;
+                r[cnt] = i;
+            }
         }
-
-        if (c1 < c0) ans += c1, x += 1 << i;
-        else ans += c0;
+        last[a[i]] = i;
     }
 
-    printf("%lld %d", ans, x);
+    for (int i = 2; i <= cnt; i ++ )
+        lg[i] = lg[i / 2] + 1;
+    for (int i = 1; i <= cnt; i ++ )
+        f[i][0] = r[i] - l[i];
+    for (int j = 1; (1 << j) <= cnt; j ++ )
+        for (int i = 1; i + (1 << j) - 1 <= cnt; i ++ )
+            f[i][j] = min(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+
+    for (int i = 1; i <= m; i ++ )
+    {
+        int ql, qr;
+        scanf("%d%d", &ql, &qr);
+        ql = lower_bound(l + 1, l + 1 + cnt, ql) - l;
+        qr = upper_bound(r + 1, r + 1 + cnt, qr) - r - 1;
+        if (ql > qr) puts("-1");
+        else printf("%d\n", ask(ql, qr));
+    }
 
     return 0;
 }
